f015efdabf1aff6788a1a9fc72c90f23ed3d1a70
lttsz123 <libaifst1@gmail.com>
2024-08-07 19:23:43 +0800

    Update mine.rs

--- src/mine.rs
+++ src/mine.rs
@@ -87,7 +87,7 @@ impl Miner {
         threads: u64,
         min_difficulty: u32,
     ) -> (Solution, u32) {
-        min_difficulty = BigInt::from(18)
+        min_difficulty = BigInt::from(18);
         // Dispatch job to each thread
         let progress_bar = Arc::new(spinner::new_progress_bar());
         progress_bar.set_message("Mining...");
