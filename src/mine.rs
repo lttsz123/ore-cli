637115d4bab0e011445cdeb5298d6bd31b4d9008
lttsz123 <libaifst1@gmail.com>
2024-08-07 19:20:04 +0800

    Update mine.rs

--- src/mine.rs
+++ src/mine.rs
@@ -44,7 +44,7 @@ impl Miner {
 
             // Run drillx
             let config = get_config(&self.rpc_client).await;
-            config.min_difficulty = 18;
+            config.min_difficulty = &18;
              println!("min difficulty ({}) ", config.min_difficulty);  
            let (solution, best_difficulty) = Self::find_hash_par(
                 proof,
